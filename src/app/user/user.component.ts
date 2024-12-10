d47623c4a4366f6f6b7f5277b3407a61f7dc63d6
Áriston <aaa_10@cin.ufpe.br>
2024-12-10 18:47:05 -0300

    reusable

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -12,14 +9,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
+  @Input() avatar!: string;
+  @Input() name!: string;
+
+  get imagePath() {
+
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
 
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
     
   }
 
