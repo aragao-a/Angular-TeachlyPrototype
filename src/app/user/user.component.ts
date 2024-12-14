41d6246a9a4e7fa3fa1a36b5ab91ced66af96d18
Áriston <aaa_10@cin.ufpe.br>
2024-12-14 14:52:55 -0300

    df

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 
 
 @Component({
@@ -10,17 +10,19 @@ import { Component, Input } from '@angular/core';
 })
 export class UserComponent {
 
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input( {required: true} ) id!: string;
+  @Input( {required: true} ) avatar!: string;
+  @Input( {required: true} ) name!: string;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
 
-    return 'assets/users/' + this.avatar;
+    return ('assets/users/' + this.avatar);
   }
 
   onSelectUser() {
 
-    
+    this.select.emit(this.id);
   }
 
 }
